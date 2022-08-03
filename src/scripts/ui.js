f8ced967feb5fe4905c620c8d252de0634d8fbdb
Ahmed <ahmedmahmoudkzm@gmail.com>
2022-08-04 00:12:07 +0200

    Finished basic page html and styling

--- src/scripts/ui.js
+++ src/scripts/ui.js
@@ -1,20 +1,32 @@
 import Sidebar from '../assets/images/side-bar.png';
 import Plus from '../assets/images/plus.png';
+import Calendar from '../assets/images/calendar.png';
+import Project from '../assets/images/projects.png';
 import '../styles/style.css';
 
-const sidebar = new Image();
-const plus = new Image();
+const sidebarIcon = new Image();
+const plusIcon = new Image();
+const calendarIcon = new Image();
+const projectsIcon = new Image();
 const projects = document.getElementById('projects');
 const header = document.querySelector('header');
+const sidebar = document.getElementById('side-bar');
+const one = document.getElementById('one');
+const two = document.getElementById('two');
 
-sidebar.src = Sidebar;
-plus.src = Plus;
+sidebarIcon.src = Sidebar;
+plusIcon.src = Plus;
+calendarIcon.src = Calendar;
+projectsIcon.src = Project;
 
-sidebar.id = 'sidebar-icn';
+plusIcon.id = 'plus-icon-side';
 
-sidebar.addEventListener('click', () => {
-    sidebar.classList.toggle('show');
+sidebarIcon.addEventListener('click', () => {
+    sidebarIcon.classList.toggle('show');
+    sidebar.classList.toggle('slide');
 });
 
-projects.appendChild(plus);
-header.appendChild(sidebar);
+projects.appendChild(plusIcon);
+header.appendChild(sidebarIcon);
+one.appendChild(calendarIcon);
+two.appendChild(projectsIcon);
