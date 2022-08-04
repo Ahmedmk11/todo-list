dcb5a50b0564015da5da14dd25e585382e4fec30
Ahmed <ahmedmahmoudkzm@gmail.com>
2022-08-04 02:51:09 +0200

    Fixed overflow issues, added more css

--- src/scripts/ui.js
+++ src/scripts/ui.js
@@ -2,22 +2,26 @@ import Sidebar from '../assets/images/side-bar.png';
 import Plus from '../assets/images/plus.png';
 import Calendar from '../assets/images/calendar.png';
 import Project from '../assets/images/projects.png';
+import List from '../assets/images/list.png';
 import '../styles/style.css';
 
 const sidebarIcon = new Image();
 const plusIcon = new Image();
 const calendarIcon = new Image();
 const projectsIcon = new Image();
+const listIcon = new Image();
 const projects = document.getElementById('projects');
 const header = document.querySelector('header');
 const sidebar = document.getElementById('side-bar');
 const one = document.getElementById('one');
 const two = document.getElementById('two');
+const all = document.getElementById('all');
 
 sidebarIcon.src = Sidebar;
 plusIcon.src = Plus;
 calendarIcon.src = Calendar;
 projectsIcon.src = Project;
+listIcon.src = List;
 
 plusIcon.id = 'plus-icon-side';
 
@@ -30,3 +34,4 @@ projects.appendChild(plusIcon);
 header.appendChild(sidebarIcon);
 one.appendChild(calendarIcon);
 two.appendChild(projectsIcon);
+all.appendChild(listIcon);
